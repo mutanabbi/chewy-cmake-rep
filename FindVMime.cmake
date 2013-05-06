a39c05393d954908292416f9550223a058f690aa
zaufi <i.zaufi@gmail.com>
2013-05-06 21:59:33 +0400

    update modules (and fix some typo)

--- FindVMime.cmake
+++ FindVMime.cmake
@@ -1,22 +1,56 @@
+# - Find VMime package using `pkg-config`
+# Search for VMime package and set the following variables:
+#  VMime_FOUND        - is package found
+#  VMime_VERSION      - found package version
+#  VMime_INCLUDE_DIRS - dir w/ header files
+#  VMime_DEFINITIONS  - other than `-I' compiler flags
+#  VMime_LIBRARIES    - libs for dynamic linkage
+#
+
+#=============================================================================
 # Copyright 2010 by Alex Turbov <i.zaufi@gmail.com>
 #
-# - Find VMime library.
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file LICENSE for details.
 #
-# Search for VMime library and set variables:
-#
-#  VMime_FOUND        - is library found.
-#  VMime_LIBRARIES    - libs for dynamic linkage.
-#  VMime_INCLUDE_DIRS - dir w/ header files.
-#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distribute this file outside of this repository, substitute the full
+#  License text for the above reference.)
+
+if(VMime_FIND_QUIETLY)
+    set(_pkg_find_quietly QUIET)
+endif()
+
+set(_pkg_module_name "vmime")
+if(VMime_FIND_VERSION)
+    if(VMime_FIND_VERSION_EXACT)
+        set(_pkg_module_name "${_pkg_module_name}=${VMime_FIND_VERSION}")
+    else()
+        set(_pkg_module_name "${_pkg_module_name}>=${VMime_FIND_VERSION}")
+    endif()
+endif()
+
+find_package(PkgConfig ${_pkg_find_quietly})
+pkg_check_modules(VMime ${_pkg_module_name} ${_pkg_find_quietly})
 
 include(FindPackageHandleStandardArgs)
-include(FindPkgConfig)
+find_package_handle_standard_args(
+    VMime
+    FOUND_VAR VMime_FOUND
+    REQUIRED_VARS VMime_LIBRARIES
+    VERSION_VAR VMime_VERSION
+  )
 
-pkg_check_modules(VMime vmime)
-
-find_package_handle_standard_args(VMime DEFAULT_MSG VMime_LIBRARIES)
+if(VMime_FOUND)
+    # Copy other than `-I' flags to `XXX_DEFINITIONS' variable,
+    # according CMake guide (/usr/share/cmake/Modules/readme.txt)
+    set(VMime_DEFINITIONS ${VMime_CFLAGS_OTHER})
+endif()
 
 # X-Chewy-RepoBase: https://raw.github.com/mutanabbi/chewy-cmake-rep/master/
 # X-Chewy-Path: FindVMime.cmake
-# X-Chewy-Version: 1.0
+# X-Chewy-Version: 1.2
 # X-Chewy-Description: Find VMime librarary using `pkg-config`
