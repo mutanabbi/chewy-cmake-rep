0fb6dfba9beb0c9a4a48734fb41b494983b2e4af
Alex Turbov <i.zaufi@gmail.com>
2014-04-02 11:57:35 +0400

    Fix description copy'n'paste typo

--- FindZooKeeperC.cmake
+++ FindZooKeeperC.cmake
@@ -79,5 +79,5 @@ endif()
 # X-Chewy-RepoBase: https://raw.github.com/mutanabbi/chewy-cmake-rep/master/
 # X-Chewy-Path: FindZooKeeperC.cmake
 # X-Chewy-Version: 1.0
-# X-Chewy-Description: Find OTL header
+# X-Chewy-Description: Find Apache ZooKeeper C API libraries
 # X-Chewy-AddonFile: zookeeper_get_version.cc
