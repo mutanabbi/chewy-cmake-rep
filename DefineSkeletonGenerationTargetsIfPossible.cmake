c6366e84cdbe88124441e5c35b22eea0a8f09fb6
Alex Turbov <i.zaufi@gmail.com>
2015-07-08 19:26:06 +0300

    improve new-class files generator

--- DefineSkeletonGenerationTargetsIfPossible.cmake
+++ DefineSkeletonGenerationTargetsIfPossible.cmake
@@ -207,7 +207,7 @@ endfunction()
 
 # X-Chewy-RepoBase: https://raw.githubusercontent.com/mutanabbi/chewy-cmake-rep/master/
 # X-Chewy-Path: DefineSkeletonGenerationTargetsIfPossible.cmake
-# X-Chewy-Version: 6.11
+# X-Chewy-Version: 6.12
 # X-Chewy-Description: Add targets to generate class header/implementation and unit-tests skeleton files
 # X-Chewy-AddonFile: TestCMakeLists.txt.in
 # X-Chewy-AddonFile: class.tpl.in
