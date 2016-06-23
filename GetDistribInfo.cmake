95f80cc9afa6afc8f4f55b8ed6683100cc6f8224
Alex Turbov <i.zaufi@gmail.com>
2016-06-24 01:51:16 +0700

    polish output

--- GetDistribInfo.cmake
+++ GetDistribInfo.cmake
@@ -7,7 +7,7 @@
 #   DISTRIB_ARCH        -- a distribution target machine
 #   DISTRIB_CODENAME    -- a distribution code name (like "quantal" or "trusty" for Ubuntu)
 #   DISTRIB_VERSION     -- a version string of the dictribution
-#   DISTRIB_PKG_FMT     -- native package manager's format suitable to use w/ CPACK_GENERATOR
+#   DISTRIB_PKG_FMT     -- native package manager's format(s) suitable to use w/ CPACK_GENERATOR
 #   DISTRIB_SRC_PKG_FMT -- native format to create tarballs
 #   DISTRIB_FILE_PART   -- a string suitable to be a filename part to identify a target system
 #
@@ -32,31 +32,23 @@ set(DEFAULT_DISTRIB_CODENAME "auto" CACHE STRING "Target distribution codename")
 set(DEFAULT_DISTRIB_ID "auto" CACHE STRING "Target distribution")
 
 #
-# ATTENTION Macro arguments are not variables!
-# So they can't be checked just w/ `if(PARAM)`!
-#
-macro(_clue_file_part PART DELIMITER STATE OUTPUT)
-    if(NOT STATE STREQUAL "")
-        if(NOT PART STREQUAL "")
-            set(${OUTPUT} "${STATE}${DELIMITER}${PART}")
-        else()
-            set(${OUTPUT} "${STATE}")
-        endif()
-    elseif(NOT PART STREQUAL "")
-        set(${OUTPUT} "${PART}")
-    endif()
-endmacro()
-
-#
 # Form a filename part for the current distro
 #
 macro(_make_distrib_file_part)
-    _clue_file_part("${DISTRIB_ID}" "" "" DISTRIB_FILE_PART)
+    string(APPEND DISTRIB_FILE_PART "${DISTRIB_ID}")
     if(WIN32)
-        _clue_file_part("${DISTRIB_ARCH}" "" "${DISTRIB_FILE_PART}" DISTRIB_FILE_PART)
+        string(APPEND DISTRIB_FILE_PART "${DISTRIB_ARCH}")
     else()
-        _clue_file_part("${DISTRIB_VERSION}" "" "${DISTRIB_FILE_PART}" DISTRIB_FILE_PART)
-        _clue_file_part("${DISTRIB_ARCH}" "-" "${DISTRIB_FILE_PART}" DISTRIB_FILE_PART)
+        if(DISTRIB_VERSION)
+            string(REPLACE "." ";" DISTRIB_VERSION_LIST "${DISTRIB_VERSION}")
+            list(GET DISTRIB_VERSION_LIST 0 DISTRIB_VERSION_MAJOR)
+            list(GET DISTRIB_VERSION_LIST 1 DISTRIB_VERSION_MINOR)
+            list(GET DISTRIB_VERSION_LIST 2 DISTRIB_VERSION_PATCH)
+            string(APPEND DISTRIB_FILE_PART "${DISTRIB_VERSION_MAJOR}.${DISTRIB_VERSION_MINOR}")
+        endif()
+        if(DISTRIB_ARCH)
+            string(APPEND DISTRIB_FILE_PART "-${DISTRIB_ARCH}")
+        endif()
     endif()
     # Make a string suitable to be a filename part to identify a target system
     string(TOLOWER "${DISTRIB_FILE_PART}" DISTRIB_FILE_PART)
@@ -96,11 +88,18 @@ macro(_try_check_redhat _release_string)
     endif()
 endmacro()
 
+function(_debug msg)
+    if(GDI_DEBUG)
+        message(STATUS "[GDI] ${msg}")
+    endif()
+endfunction()
+
 #
 # Ok, lets collect come info about this distro...
 #
-if(NOT DISTRIB_CODENAME)
+if(NOT DISTRIB_ID)
     if(NOT WIN32)
+        _debug("We r not in Windows! Trying `uname`")
         find_program(
             UNAME_EXECUTABLE
             NAMES uname
@@ -114,23 +113,29 @@ if(NOT DISTRIB_CODENAME)
                 ERROR_QUIET
               )
             # NOTE DISTRIB_ARCH can be overriden (tuned) later
+            _debug("`uname -m` returns ${DISTRIB_ARCH}")
         endif()
     endif()
 
     # Trying Windows
     if(WIN32)
-
+        _debug("Windows detected. Checking `void*` size")
+        set(DISTRIB_ID "Win")
         set(DISTRIB_PKG_FMT "WIX")
         set(DISTRIB_SRC_PKG_FMT "ZIP")
-        if(MSVC)
-            set(DISTRIB_ID "Win")
-            if(CMAKE_CL_64)
-                set(DISTRIB_ARCH "64")
-                list(APPEND DISTRIB_PKG_FMT "NSIS64")
-            else()
-                set(DISTRIB_ARCH "32")
-                list(APPEND DISTRIB_PKG_FMT "NSIS")
-            endif()
+        set(DISTRIB_VERSION "${CMAKE_SYSTEM_VERSION}")
+
+        if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+            _debug("  `void*` size is 8")
+            set(DISTRIB_ARCH "64")
+            list(APPEND DISTRIB_PKG_FMT "NSIS64")
+        elseif(CMAKE_SIZEOF_VOID_P EQUAL 4)
+            _debug("  `void*` size is 4")
+            set(DISTRIB_ARCH "32")
+            list(APPEND DISTRIB_PKG_FMT "NSIS")
+        elseif(NOT CMAKE_SIZEOF_VOID_P)
+            _debug("  `void*` size is undefined")
+            set(DISTRIB_ARCH "-noarch")
         endif()
 
     # Trying LSB conformant distros like Ubuntu, RHEL or CentOS w/
@@ -236,17 +241,30 @@ if(NOT DISTRIB_CODENAME)
                 endif()
             endif()
         endif()
-    else()
-        set(DISTRIB_ID "${DEFAULT_DISTRIB_ID}" CACHE INTERNAL "Target distribution")
-        set(DISTRIB_CODENAME "${DEFAULT_DISTRIB_CODENAME}" CACHE INTERNAL "Target distribution codename")
     endif()
 
     _make_distrib_file_part()
 
-    message(STATUS "Target distribution: ${DISTRIB_ID} ${DISTRIB_VERSION} [${DISTRIB_FILE_PART}]")
+    if(DISTRIB_ID)
+        set(_distrib_info_line "${DISTRIB_ID}")
+
+        if(DISTRIB_VERSION)
+            set(_distrib_info_line "${_distrib_info_line} ${DISTRIB_VERSION}")
+        endif()
+
+        if(DISTRIB_FILE_PART)
+            set(_distrib_info_line "${_distrib_info_line} [${DISTRIB_FILE_PART}]")
+        endif()
+
+        message(STATUS "Target distribution: ${_distrib_info_line}")
+
+        unset(_distrib_info_line)
+    else()
+        message(STATUS "Target distribution is unknown")
+    endif()
 endif()
 
 # X-Chewy-RepoBase: https://raw.githubusercontent.com/mutanabbi/chewy-cmake-rep/master/
 # X-Chewy-Path: GetDistribInfo.cmake
-# X-Chewy-Version: 2.14
+# X-Chewy-Version: 2.20
 # X-Chewy-Description: Get a distribution codename
