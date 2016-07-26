4595c6629f55bc834bedeac71e5543bd135eb83c
Alex Turbov <i.zaufi@gmail.com>
2016-07-26 20:12:07 +0300

    a major improvement to store ABI dumps at JFrog Artifactory

--- AddABICheckTarget.cmake
+++ AddABICheckTarget.cmake
@@ -1,6 +1,7 @@
 # - Add targets to update ABI dump and check ABI compatibility
 #
 # TODO Docs
+# TODO Make it possible to store ABI dumps at arbitrary service
 #
 # Simple example:
 # add_abi_check_target(
@@ -13,7 +14,7 @@
 #
 
 #=============================================================================
-# Copyright 2015 by Alex Turbov <i.zaufi@gmail.com>
+# Copyright 2015-2016 by Alex Turbov <i.zaufi@gmail.com>
 #
 # Distributed under the OSI-approved BSD License (the "License");
 # see accompanying file LICENSE for details.
@@ -28,16 +29,16 @@
 
 include(CMakeParseArguments)
 
-find_program(ABI_COMPIANCE_CHECKER_EXECUABLE abi-compliance-checker)
-if(ABI_COMPIANCE_CHECKER_EXECUABLE)
-    message(STATUS "Found abi-compliance-checker: ${ABI_COMPIANCE_CHECKER_EXECUABLE}")
+find_program(ABI_COMPLIANCE_CHECKER_EXECUTABLE abi-compliance-checker)
+if(ABI_COMPLIANCE_CHECKER_EXECUTABLE)
+    message(STATUS "Found abi-compliance-checker: ${ABI_COMPLIANCE_CHECKER_EXECUTABLE}")
 else()
     message(STATUS "WARNING: `abi-compliance-checker` not found. Won't check ABI compliance...")
 endif()
 
-find_program(JQ_EXECUABLE jq)
-if(JQ_EXECUABLE)
-    message(STATUS "Found jq: ${JQ_EXECUABLE}")
+find_program(JQ_EXECUTABLE jq)
+if(JQ_EXECUTABLE)
+    message(STATUS "Found jq: ${JQ_EXECUTABLE}")
 else()
     message(STATUS "WARNING: `jq` not found. Won't check ABI compliance...")
 endif()
@@ -58,7 +59,7 @@ set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 function(add_abi_check_target)
     set(options DEBUG)
-    set(one_value_args TARGET DIRECTORY LIBRARY OUTPUT VERSION)
+    set(one_value_args ARTIFACTORY_REPO ARTIFACTORY_USER ARTIFACTORY_PASS DIRECTORY TARGET VERSION)
     set(multi_value_args HEADERS SKIP_HEADERS SOURCES)
     cmake_parse_arguments(add_abi_check_target "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
 
@@ -76,21 +77,49 @@ function(add_abi_check_target)
     endif()
 
     # Check that `jq` is here...
-    if(NOT JQ_EXECUABLE)
+    if(NOT JQ_EXECUTABLE)
         if(add_abi_check_target_DEBUG)
             message(STATUS "  [add_abi_check_target] `jq` wasn't found. Exiting...")
         endif()
         return()
     endif()
 
+    if(NOT add_abi_check_target_ARTIFACTORY_REPO)
+        message(FATAL_ERROR "No `ARTIFACTORY_REPO` has been given in call to `add_abi_check_target()`")
+    endif()
+
     # `TARGET` is a mandatory option
     if(NOT TARGET ${add_abi_check_target_TARGET})
         message(FATAL_ERROR "`${add_abi_check_target_TARGET}' is not a target name")
     endif()
-    # TODO Make sure target is a library?
+    get_target_property(_is_imported ${add_abi_check_target_TARGET} IMPORTED)
+    if(_is_imported)
+        message(FATAL_ERROR "Target `${add_abi_check_target_TARGET}' should not be imported one")
+    endif()
+    get_target_property(_target_type ${add_abi_check_target_TARGET} TYPE)
+    if(_target_type STREQUAL "MODULE_LIBRARY" OR _target_type STREQUAL "SHARED_LIBRARY")
+        # No additional options needed
+    elseif(_target_type STREQUAL "STATIC_LIBRARY")
+        set(add_abi_check_target_ABI_CHECK_OPTIONS "-static-libs")
+    else()
+        message(FATAL_ERROR "Target `${add_abi_check_target_TARGET}` has unsupported type ${_target_type}")
+    endif()
+
+    # Preset some options for ABI checker
+    set(add_abi_check_target_ABI_CHECKER_DIR "${CMAKE_CURRENT_BINARY_DIR}/${add_abi_check_target_TARGET}-abi-checker")
+    file(MAKE_DIRECTORY "${add_abi_check_target_ABI_CHECKER_DIR}")
 
     # Prepare target properties for future including
-    set(add_abi_check_target_TARGET_PROPS_FILE "${CMAKE_CURRENT_BINARY_DIR}/${add_abi_check_target_TARGET}-abi-check-target-properties.cmake")
+    # ATTENTION CMake protect accessing `LOCATION` property of targets, so it is really
+    # (impossible) to get a full path and a filename of it in currently executed `CMakeLists.txt`.
+    # So here is a trick: render a `*.cmake` file w/ needed properties and include it at the moment
+    # of custom targets execution.
+    # ATTENTION At the very first time, a file generated is not immediatly flushed!
+    # So, do not even try to `include()` it right after generation!
+    set(
+        add_abi_check_target_TARGET_PROPS_FILE
+        "${add_abi_check_target_ABI_CHECKER_DIR}/${add_abi_check_target_TARGET}-abi-check-target-properties.cmake"
+      )
     file(
         GENERATE
         OUTPUT "${add_abi_check_target_TARGET_PROPS_FILE}"
@@ -103,9 +132,8 @@ function(add_abi_check_target)
     if(NOT add_abi_check_target_VERSION)
         get_target_property(add_abi_check_target_VERSION ${add_abi_check_target_TARGET} SOVERSION)
     endif()
-
-    if(NOT add_abi_check_target_OUTPUT)
-        set(add_abi_check_target_OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/${add_abi_check_target_TARGET}.${add_abi_check_target_VERSION}.xml")
+    if(NOT add_abi_check_target_VERSION)
+        message(FATAL_ERROR "No `VERSION` parameter has given and no `SOVERSION` property has set on target `${add_abi_check_target_TARGET}`")
     endif()
 
     if(NOT add_abi_check_target_HEADERS)
@@ -119,54 +147,141 @@ function(add_abi_check_target)
         get_target_property(add_abi_check_target_SOURCES ${add_abi_check_target_TARGET} SOURCES)
     endif()
 
-    set(add_abi_check_target_COMMON_CMAKE_CODE "${CMAKE_CURRENT_BINARY_DIR}/${add_abi_check_target_TARGET}-abi-common.cmake")
+    set(
+        add_abi_check_target_XML_DESCRIPTOR
+        "${add_abi_check_target_ABI_CHECKER_DIR}/${add_abi_check_target_TARGET}.${add_abi_check_target_VERSION}.xml"
+      )
+
+    # Set expected ABI dump filename
+    # ALERT There is no way to get target filename at this point!
+    # So, lets guess a name...
+    # TODO Provide a way to override it via parameter.
+    set(_libname "lib${add_abi_check_target_TARGET}")
+    set(add_abi_check_target_DUMP_FILE "${add_abi_check_target_ABI_CHECKER_DIR}/${_libname}_${add_abi_check_target_VERSION}.abi.tar.gz")
+    set(add_abi_check_target_LATEST_DUMP_FILE "${add_abi_check_target_ABI_CHECKER_DIR}/${_libname}_last.abi.tar.gz")
+
+    set(add_abi_check_target_COMMON_CMAKE_CODE "${add_abi_check_target_ABI_CHECKER_DIR}/${add_abi_check_target_TARGET}-abi-common.cmake")
     configure_file(
         "${_ADD_ABI_CHECK_TARGET_LIST_FILE}.in"
         "${add_abi_check_target_COMMON_CMAKE_CODE}"
         @ONLY
       )
 
+    # Bind XML preparation to the target's post-build event, so it'll be updated
+    # every time the target has changed
     configure_file(
-        "${_ADD_ABI_CHECK_TARGET_LIST_DIR}/abi-check.cmake.in"
-        "${CMAKE_CURRENT_BINARY_DIR}/${add_abi_check_target_TARGET}-abi-check.cmake"
+        "${_ADD_ABI_CHECK_TARGET_LIST_DIR}/abi-check-prepare-xml.cmake.in"
+        "${add_abi_check_target_ABI_CHECKER_DIR}/${add_abi_check_target_TARGET}-prepare-xml.cmake"
         @ONLY
       )
-
-    configure_file(
-        "${_ADD_ABI_CHECK_TARGET_LIST_DIR}/abi-dump-update.cmake.in"
-        "${CMAKE_CURRENT_BINARY_DIR}/${add_abi_check_target_TARGET}-update-abi-dump.cmake"
-        @ONLY
+    add_custom_command(
+        TARGET ${add_abi_check_target_TARGET}
+        POST_BUILD
+        COMMAND "${CMAKE_COMMAND}" -P "${add_abi_check_target_ABI_CHECKER_DIR}/${add_abi_check_target_TARGET}-prepare-xml.cmake"
+        BYPRODUCTS "${add_abi_check_target_XML_DESCRIPTOR}"
+        COMMENT "Generate XML descriptor for `${add_abi_check_target_TARGET}` to check ABI compatibility"
       )
 
-    add_custom_target(
-        ${add_abi_check_target_TARGET}-abi-check
-        COMMAND ${CMAKE_COMMAND} -P "${CMAKE_CURRENT_BINARY_DIR}/${add_abi_check_target_TARGET}-abi-check.cmake"
-        COMMENT "Checking current version ${add_abi_check_target_VERSION} of `${add_abi_check_target_TARGET}` against latest ABI dump"
+    # Add target to produce ABI dump
+    configure_file(
+        "${_ADD_ABI_CHECK_TARGET_LIST_DIR}/abi-dump-make.cmake.in"
+        "${add_abi_check_target_ABI_CHECKER_DIR}/${add_abi_check_target_TARGET}-make-abi-dump.cmake"
+        @ONLY
+      )
+    add_custom_command(
+        OUTPUT "${add_abi_check_target_DUMP_FILE}"
+        COMMAND "${CMAKE_COMMAND}" -P "${add_abi_check_target_ABI_CHECKER_DIR}/${add_abi_check_target_TARGET}-make-abi-dump.cmake"
+        COMMENT "Generate ABI dump for `${add_abi_check_target_TARGET}`"
+        MAIN_DEPENDENCY "${add_abi_check_target_XML_DESCRIPTOR}"
         DEPENDS
             "${_ADD_ABI_CHECK_TARGET_LIST_FILE}.in"
-            "${_ADD_ABI_CHECK_TARGET_LIST_DIR}/abi-check.cmake.in"
-            "${CMAKE_CURRENT_BINARY_DIR}/${add_abi_check_target_TARGET}-abi-check.cmake"
+            "${_ADD_ABI_CHECK_TARGET_LIST_DIR}/abi-dump-make.cmake.in"
+            "${add_abi_check_target_ABI_CHECKER_DIR}/${add_abi_check_target_TARGET}-make-abi-dump.cmake"
       )
-    add_dependencies(${add_abi_check_target_TARGET}-abi-check ${add_abi_check_target_TARGET})
 
+    # Add target to upload a dump to Artifactory
+    configure_file(
+        "${_ADD_ABI_CHECK_TARGET_LIST_DIR}/abi-dump-update.cmake.in"
+        "${add_abi_check_target_ABI_CHECKER_DIR}/${add_abi_check_target_TARGET}-update-abi-dump.cmake"
+        @ONLY
+      )
     add_custom_target(
         ${add_abi_check_target_TARGET}-update-abi-dump
-        COMMAND ${CMAKE_COMMAND} -P "${CMAKE_CURRENT_BINARY_DIR}/${add_abi_check_target_TARGET}-update-abi-dump.cmake"
+        COMMAND "${CMAKE_COMMAND}" -P "${add_abi_check_target_ABI_CHECKER_DIR}/${add_abi_check_target_TARGET}-update-abi-dump.cmake"
         COMMENT "Updating latest ABI dump for `${add_abi_check_target_TARGET}`"
         DEPENDS
             "${_ADD_ABI_CHECK_TARGET_LIST_FILE}.in"
             "${_ADD_ABI_CHECK_TARGET_LIST_DIR}/abi-dump-update.cmake.in"
-            "${CMAKE_CURRENT_BINARY_DIR}/${add_abi_check_target_TARGET}-update-abi-dump.cmake"
+            "${add_abi_check_target_ABI_CHECKER_DIR}/${add_abi_check_target_TARGET}-update-abi-dump.cmake"
+            "${add_abi_check_target_DUMP_FILE}"
       )
-    add_dependencies(${add_abi_check_target_TARGET}-update-abi-dump ${add_abi_check_target_TARGET})
+
+    # Add target to get the latest ABI dump
+    configure_file(
+        "${_ADD_ABI_CHECK_TARGET_LIST_DIR}/abi-dump-get-latest.cmake.in"
+        "${add_abi_check_target_ABI_CHECKER_DIR}/${add_abi_check_target_TARGET}-get-latest-abi-dump.cmake"
+        @ONLY
+      )
+    add_custom_command(
+        OUTPUT "${add_abi_check_target_LATEST_DUMP_FILE}"
+        COMMAND "${CMAKE_COMMAND}" -P "${add_abi_check_target_ABI_CHECKER_DIR}/${add_abi_check_target_TARGET}-get-latest-abi-dump.cmake"
+        COMMENT "Getting latest ABI dump for `${add_abi_check_target_TARGET}`"
+        DEPENDS
+            "${_ADD_ABI_CHECK_TARGET_LIST_FILE}.in"
+            "${_ADD_ABI_CHECK_TARGET_LIST_DIR}/artifactory_get_dumps.aql.in"
+            "${_ADD_ABI_CHECK_TARGET_LIST_DIR}/abi-dump-get-latest.cmake.in"
+            "${add_abi_check_target_ABI_CHECKER_DIR}/${add_abi_check_target_TARGET}-get-latest-abi-dump.cmake"
+      )
+
+    # Add target to do checking
+    configure_file(
+        "${_ADD_ABI_CHECK_TARGET_LIST_DIR}/abi-check.cmake.in"
+        "${add_abi_check_target_ABI_CHECKER_DIR}/${add_abi_check_target_TARGET}-abi-check.cmake"
+        @ONLY
+      )
+    add_custom_target(
+        ${add_abi_check_target_TARGET}-abi-check
+        COMMAND "${CMAKE_COMMAND}" -P "${add_abi_check_target_ABI_CHECKER_DIR}/${add_abi_check_target_TARGET}-abi-check.cmake"
+        COMMENT "Checking current version ${add_abi_check_target_VERSION} of `${add_abi_check_target_TARGET}` against latest ABI dump"
+        BYPRODUCTS "${add_abi_check_target_ABI_CHECKER_DIR}/report.html"
+        DEPENDS
+            "${_ADD_ABI_CHECK_TARGET_LIST_FILE}.in"
+            "${_ADD_ABI_CHECK_TARGET_LIST_DIR}/abi-check.cmake.in"
+            "${add_abi_check_target_ABI_CHECKER_DIR}/${add_abi_check_target_TARGET}-abi-check.cmake"
+            "${add_abi_check_target_XML_DESCRIPTOR}"
+            "${add_abi_check_target_DUMP_FILE}"
+            "${add_abi_check_target_LATEST_DUMP_FILE}"
+            "${add_abi_check_target_TARGET}"
+      )
+    add_dependencies(${add_abi_check_target_TARGET}-abi-check ${add_abi_check_target_TARGET})
+
+    include("${_ADD_ABI_CHECK_TARGET_LIST_DIR}/TeamCityIntegration.cmake")
+    is_running_under_teamcity(_under_tc)
+    if(NOT _under_tc)
+        include("${_ADD_ABI_CHECK_TARGET_LIST_DIR}/AddOpenTarget.cmake")
+        add_open_target(
+            ${add_abi_check_target_TARGET}-show-abi-check-report
+            "${add_abi_check_target_ABI_CHECKER_DIR}/report.html"
+            DEPENDS
+                "${add_abi_check_target_ABI_CHECKER_DIR}/report.html"
+          )
+        add_dependencies(${add_abi_check_target_TARGET}-show-abi-check-report ${add_abi_check_target_TARGET}-abi-check)
+    endif()
+
 endfunction()
 
 # X-Chewy-RepoBase: https://raw.githubusercontent.com/mutanabbi/chewy-cmake-rep/master/
 # X-Chewy-Path: AddABICheckTarget.cmake
-# X-Chewy-Version: 1.4
+# X-Chewy-Version: 3.0
 # X-Chewy-Description: Use `abi-compliance-checker` from CMake build
 # X-Chewy-AddonFile: AddABICheckTarget.cmake.in
+# X-Chewy-AddonFile: AddOpenTarget.cmake
+# X-Chewy-AddonFile: Artifactory.cmake
 # X-Chewy-AddonFile: TeamCityIntegration.cmake
 # X-Chewy-AddonFile: abi-check.cmake.in
+# X-Chewy-AddonFile: abi-check-prepare-xml.cmake.in
+# X-Chewy-AddonFile: abi-dump-get-latest.cmake.in
+# X-Chewy-AddonFile: abi-dump-make.cmake.in
 # X-Chewy-AddonFile: abi-dump-update.cmake.in
+# X-Chewy-AddonFile: artifactory_get_dumps.aql
 # X-Chewy-AddonFile: library-abi.xml.in
---
+++ Artifactory.cmake
@@ -0,0 +1,280 @@
+# - Functions to talk to JFrog Artifactory via REST API
+#
+# TODO Docs
+#
+
+#=============================================================================
+# Copyright 2016 by Alex Turbov <i.zaufi@gmail.com>
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file LICENSE for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distribute this file outside of this repository, substitute the full
+#  License text for the above reference.)
+
+find_program(CURL_EXECUTABLE curl)
+
+set(_ARTIFACTORY_LIST_DIR "${CMAKE_CURRENT_LIST_DIR}")
+
+function(artifactory_send_files)
+    if(NOT CURL_EXECUTABLE)
+        message(FATAL_ERROR "`curl` not found, but required for sending to Artifactory")
+    endif()
+
+    set(_options)
+    set(_one_value_args PASS TIMEOUT URL_TEMPLATE USER WORKING_DIRECTORY)
+    set(_multi_value_args FILES PROPERTIES)
+    cmake_parse_arguments(_artifactory_send_files "${_options}" "${_one_value_args}" "${_multi_value_args}" ${ARGN})
+
+    if(NOT _artifactory_send_files_URL_TEMPLATE)
+        message(FATAL_ERROR "`URL_TEMPLATE` parameter is required")
+    elseif(NOT _artifactory_send_files_URL_TEMPLATE MATCHES "%\\(file\\)")
+        message(FATAL_ERROR "`URL_TEMPLATE='${_artifactory_send_files_URL_TEMPLATE}'` do not contain `%(file)` placeholder")
+    endif()
+
+    if(NOT _artifactory_send_files_FILES)
+        message(FATAL_ERROR "`FILES` parameter is required")
+    endif()
+
+    if(NOT _artifactory_send_files_WORKING_DIRECTORY)
+        set(_artifactory_send_files_WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}")
+    endif()
+
+    if(_artifactory_send_files_USER AND _artifactory_send_files_PASS)
+        set(_credentials "-u" "${_artifactory_send_files_USER}:${_artifactory_send_files_PASS}")
+    else()
+        if(EXISTS "$ENV{HOME}/.artifactoryrc.cmake")
+            include("$ENV{HOME}/.artifactoryrc.cmake" RESULT_VARIABLE _artrc)
+            if(_artrc)
+                if(DEFINED USER AND DEFINED PASS)
+                    set(_credentials "-u" "${USER}:${PASS}")
+                endif()
+            endif()
+        endif()
+    endif()
+
+    if(_artifactory_send_files_PROPERTIES)
+        set(_artifactory_send_files_PROPERTIES ";${_artifactory_send_files_PROPERTIES}")
+    endif()
+
+    if(NOT _artifactory_send_files_TIMEOUT)
+        set(_artifactory_send_files_TIMEOUT "20")
+    endif()
+
+    # Listen to `VERBOSE` environment variable -- it'll provided by `make`
+    # if user requested verbose output...
+    if("$ENV{VERBOSE}")
+        set(_verbose "-v" "-s")
+    endif()
+
+    foreach(_file IN LISTS _artifactory_send_files_FILES)
+        # Form the final URI
+        get_filename_component(_filename "${_file}" NAME)
+        string(REPLACE "%(file)" "${_filename}" _url "${_artifactory_send_files_URL_TEMPLATE}")
+
+        file(MD5 "${_file}" _md5_sum)
+        if(_md5_sum)
+            list(APPEND _headers "-H" "X-Checksum-Md5: ${_md5_sum}")
+        endif()
+        file(SHA1 "${_file}" _sha1_sum)
+        if(_sha1_sum)
+            list(APPEND _headers "-H" "X-Checksum-Sha1: ${_sha1_sum}")
+        endif()
+
+        # Send it!
+        execute_process(
+            COMMAND "${CURL_EXECUTABLE}"
+                ${_verbose}
+                ${_credentials}
+                -m ${_artifactory_send_files_TIMEOUT}
+                ${_headers}
+                -T "${_file}"
+                "${_url}${_artifactory_send_files_PROPERTIES}"
+            WORKING_DIRECTORY "${_artifactory_send_files_WORKING_DIRECTORY}"
+            RESULT_VARIABLE _curl_rc
+          )
+        if(NOT _curl_rc EQUAL 0)
+            message(FATAL_ERROR "Error while uploading file `${_file}`")
+        endif()
+    endforeach()
+
+endfunction()
+
+
+function(artifactory_get_latest_abi_dump OUTPUT_FILENAME)
+    set(_options)
+    set(_one_value_args FILENAME_GLOB PASS PATH REPOSITORY_URL RESULT_VARIABLE TIMEOUT USER WORKING_DIRECTORY)
+    set(_multi_value_args)
+    cmake_parse_arguments(_artifactory_get_latest_abi_dump "${_options}" "${_one_value_args}" "${_multi_value_args}" ${ARGN})
+
+    # Verify mandatory parameters
+    # - `OUTPUT_FILENAME` is the must
+    if(NOT OUTPUT_FILENAME)
+        message(FATAL_ERROR "No output filename given in call to `artifactory_get_latest_abi_dump()`")
+    endif()
+    # - w/o `REPOSITORY_URL` there is nothing to do
+    if(NOT _artifactory_get_latest_abi_dump_REPOSITORY_URL)
+        message(FATAL_ERROR "`REPOSITORY_URL` parameter is required")
+    endif()
+    if(NOT _artifactory_get_latest_abi_dump_FILENAME_GLOB)
+        message(FATAL_ERROR "`FILENAME_GLOB` parameter is required")
+    endif()
+
+    # if `WORKING_DIRECTORY` is omitted, suppose it is `CMAKE_CURRENT_BINARY_DIR`
+    if(NOT _artifactory_get_latest_abi_dump_WORKING_DIRECTORY)
+        set(_artifactory_get_latest_abi_dump_WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}")
+    endif()
+
+    if(NOT _artifactory_get_latest_abi_dump_TIMEOUT)
+        set(_artifactory_get_latest_abi_dump_TIMEOUT "20")
+    endif()
+
+    # Prepare parameter for `-u` CLI option:
+    # in case when no user+pass explicitly has given, try to read it from
+    # per user "configuration" file: `~/.artifactoryrc.cmake`
+    if(_artifactory_get_latest_abi_dump_USER AND _artifactory_get_latest_abi_dump_PASS)
+        set(_credentials "-u" "${_artifactory_get_latest_abi_dump_USER}:${_artifactory_get_latest_abi_dump_PASS}")
+    else()
+        if(EXISTS "$ENV{HOME}/.artifactoryrc.cmake")
+            include("$ENV{HOME}/.artifactoryrc.cmake" RESULT_VARIABLE _artrc)
+            if(_artrc)
+                if(DEFINED USER AND DEFINED PASS)
+                    set(_credentials "-u" "${USER}:${PASS}")
+                endif()
+            endif()
+        endif()
+    endif()
+
+    # Listen to `VERBOSE` environment variable -- it'll provided by `make`
+    # if user requested verbose output...
+    if("$ENV{VERBOSE}")
+        set(_verbose "-v" "-s")
+    endif()
+
+    # Produce a search URL and repository name from a base repo URL.
+    # ATTENTION It is important that `REPOSITORY_URL` do not have a trailing slash!
+    # TODO Strip '/' if present!
+    get_filename_component(
+        _artifactory_get_latest_abi_dump_REPOSITORY
+        "${_artifactory_get_latest_abi_dump_REPOSITORY_URL}"
+        NAME
+      )
+    string(REPLACE "/${_artifactory_get_latest_abi_dump_REPOSITORY}"
+        ""
+        _artifactory_get_latest_abi_dump_SEARCH_URL
+        "${_artifactory_get_latest_abi_dump_REPOSITORY_URL}"
+      )
+    string(APPEND _artifactory_get_latest_abi_dump_SEARCH_URL "/api/search/aql")
+
+    # ATTENTION It is important that `PATH` do not have a trailing slash!
+    # TODO Strip '/' if present!
+
+    # Ok, render AQL to be send
+    configure_file(
+        "${_ARTIFACTORY_LIST_DIR}/artifactory_get_dumps.aql.in"
+        "${CMAKE_CURRENT_BINARY_DIR}/${add_abi_check_target_TARGET}_get_dumps.aql"
+        @ONLY
+      )
+
+    # Search for ABI dumps at specified repository and get `version` and `name` properties
+    # transforming from JSON into a CSV format. If no files found, return `NOTFOUND` which
+    # is synonym of `false` for CMake.
+    execute_process(
+        COMMAND "${CURL_EXECUTABLE}"
+            ${_verbose}
+            ${_credentials}
+            -m "${_artifactory_get_latest_abi_dump_TIMEOUT}"
+            -H "Content-Type: application/json"
+            -d "@${CMAKE_CURRENT_BINARY_DIR}/${add_abi_check_target_TARGET}_get_dumps.aql"
+            "${_artifactory_get_latest_abi_dump_SEARCH_URL}"
+        COMMAND "${JQ_EXECUTABLE}" "-r"
+            "if .range.total == 0 then \"NOTFOUND\" else .results[]|[.properties[].value, .name]|join(\",\") end"
+        WORKING_DIRECTORY "${_artifactory_get_latest_abi_dump_WORKING_DIRECTORY}"
+        OUTPUT_VARIABLE _abi_dumps_list
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+        RESULT_VARIABLE _curl_rc
+      )
+    # Make sure `curl` has no errors...
+    if(NOT _curl_rc EQUAL 0)
+        if(_artifactory_get_latest_abi_dump_RESULT_VARIABLE)
+            message(WARNING "Error while querying Artifactory server")
+            set(${_artifactory_get_latest_abi_dump_RESULT_VARIABLE} -400 PARENT_SCOPE)
+            return()
+        else()
+            message(FATAL_ERROR "Error while querying Artifactory server")
+        endif()
+    endif()
+    # ... and we've got some results
+    if(NOT _abi_dumps_list)
+        if(_artifactory_get_latest_abi_dump_RESULT_VARIABLE)
+            message(WARNING "No ABI dumps found at Atrifactory storage")
+            set(${_artifactory_get_latest_abi_dump_RESULT_VARIABLE} 1 PARENT_SCOPE)
+            return()
+        else()
+            message(FATAL_ERROR "No ABI dumps found at Atrifactory storage")
+        endif()
+    endif()
+
+    # Initialize expected results
+    set(_selected_version 0.0.0)
+    set(_selected_file)
+
+    # Transform lines into a list and check what we've got
+    string(REPLACE "\n" ";" _abi_dumps_list "${_abi_dumps_list}")
+    foreach(_line IN LISTS _abi_dumps_list)
+        if(_line MATCHES "([^,]+),(.*)")
+            set(_abi_dump_version "${CMAKE_MATCH_1}")
+            set(_abi_dump_file "${CMAKE_MATCH_2}")
+            if(_selected_version VERSION_LESS _abi_dump_version)
+                set(_selected_version "${_abi_dump_version}")
+                set(_selected_file "${_abi_dump_file}")
+            endif()
+        else()
+            if(_artifactory_get_latest_abi_dump_RESULT_VARIABLE)
+                message(WARNING "Unexpected response: `${_line}`")
+                set(${_artifactory_get_latest_abi_dump_RESULT_VARIABLE} -100 PARENT_SCOPE)
+                return()
+            else()
+                message(FATAL_ERROR "Unexpected response: `${_line}`")
+            endif()
+        endif()
+    endforeach()
+
+    if(NOT _selected_file)
+        # TODO Need `assert()` in CMake
+        # Normally it shouldn't happened... only if the loop above is totally broken
+        message(FATAL_ERROR "No ABI dump has selected")
+    endif()
+
+    # Ok, we've got a winner, lets download it
+    set(_url "${_artifactory_get_latest_abi_dump_REPOSITORY_URL}")
+    if(_artifactory_get_latest_abi_dump_PATH)
+        string(APPEND _url "/${_artifactory_get_latest_abi_dump_PATH}")
+    endif()
+    string(APPEND _url "/${_selected_file}")
+
+    execute_process(
+        COMMAND "${CURL_EXECUTABLE}"
+            ${_verbose}
+            ${_credentials}
+            -m "${_artifactory_get_latest_abi_dump_TIMEOUT}"
+            -o "${OUTPUT_FILENAME}"
+            "${_url}"
+        WORKING_DIRECTORY "${_artifactory_get_latest_abi_dump_WORKING_DIRECTORY}"
+        RESULT_VARIABLE _curl_rc
+      )
+    if(NOT _curl_rc EQUAL 0)
+        if(_artifactory_get_latest_abi_dump_RESULT_VARIABLE)
+            message(WARNING "Error while retrieving latest ABI dump from `${_url}`")
+            set(${_artifactory_get_latest_abi_dump_RESULT_VARIABLE} -401 PARENT_SCOPE)
+            return()
+        else()
+            message(FATAL_ERROR "Error while retrieving latest ABI dump from `${_url}`")
+        endif()
+    endif()
+    set(${_artifactory_get_latest_abi_dump_RESULT_VARIABLE} 0 PARENT_SCOPE)
+endfunction()
